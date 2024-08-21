244a00ea37bf30d94897efff5e74113afc66a58f
Mahendra Lingampalli <166372037+Mahendralingampalli@users.noreply.github.com>
2024-08-21 22:20:25 +0530

    Program to calculate a CGPA

---
+++ cgpa.java
@@ -0,0 +1,14 @@
+
+public class cgpa {
+    public static void main(String[] args) {
+        float operatingSystem=55;
+        float java=61;
+        float dataMining=55;
+       
+
+        float CGPA=(operatingSystem+java+dataMining)/30;
+        System.out.println(CGPA);
+    }
+
+    
+}
